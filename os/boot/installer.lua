4ae6877d406725e4c90094b05dadf1983a512148
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-01-11 16:25:45 -0500

    Update installer.lua

--- os/boot/installer.lua
+++ os/boot/installer.lua
@@ -21,31 +21,32 @@ if not fs.exists("/boot/boot_1.lua") then
     print("ECoreOS Installer\n")
     print("Would you like to install ECoreOS? (Y/n)\n")
     while true do
-    write("> ")
-    local input = read()
-    if string.lower(input) == "y" then
-        print("Installing files...")
-        pre_install("os/startup.lua", "/startup.lua")
-        pre_install("os/boot/boot_1.lua", "/boot/boot_1.lua")
-        pre_install("os/boot/configuration.lua", "/boot/configuration.lua")
-        pre_install("os/boot/installer.lua", "/boot/installer.lua")
-        pre_install("os/apis/ecore_gui.lua", "/apis/ecore_gui.lua")
-        pre_install("os/apis/konfig.lua", "/apis/konfig.lua")
-        konfig = require("/apis/konfig.lua")
-        print("Setting default configuration...")
-        konfig.set("require_modem", false)
-        konfig.set("require_host", false)
-        konfig.set("host_id", 0)
-        print("Installation complete! Rebooting...")
-        sleep(3)
-        os.reboot()
-    elseif string.lower(input) == "n" then
-        term.clear()
-        term.setCursorPos(1,1)
-        print("Installation cancelled!")
-        break
-    else
-        print("Invalid input. Please try again!")
+        write("> ")
+        local input = read()
+        if string.lower(input) == "y" then
+            print("Installing files...")
+            pre_install("os/startup.lua", "/startup.lua")
+            pre_install("os/boot/boot_1.lua", "/boot/boot_1.lua")
+            pre_install("os/boot/configuration.lua", "/boot/configuration.lua")
+            pre_install("os/boot/installer.lua", "/boot/installer.lua")
+            pre_install("os/apis/ecore_gui.lua", "/apis/ecore_gui.lua")
+            pre_install("os/apis/konfig.lua", "/apis/konfig.lua")
+            konfig = require("/apis/konfig.lua")
+            print("Setting default configuration...")
+            konfig.set("require_modem", false)
+            konfig.set("require_host", false)
+            konfig.set("host_id", 0)
+            print("Installation complete! Rebooting...")
+            sleep(3)
+            os.reboot()
+        elseif string.lower(input) == "n" then
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Installation cancelled!")
+            break
+        else
+            print("Invalid input. Please try again!")
+        end
     end
 end
 

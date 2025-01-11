d30e932dac735c6b4e199d94e4bcf776afac35e5
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-01-11 15:45:13 -0500

    Create konfig.lua

---
+++ apis/konfig.lua
@@ -0,0 +1,70 @@
+local konfig = {}
+
+function konfig.getAll()
+    if not fs.exists(".konfig_settings") then
+        local f = fs.open(".konfig_settings", "w")
+        f.write("{}")
+        f.close()
+    end
+    local f = fs.open(".konfig_settings", "r")
+    local data = f.readAll()
+    local final = textutils.unserialise(data)
+    f.close()
+
+    return final
+end
+
+function konfig.get(setting)
+    local data = konfig.getAll()
+
+    for _,s in pairs(data) do
+        if s.name == setting then
+            return s.value
+        end
+    end
+
+    return nil
+end
+
+function konfig.set(setting, newValue)
+    local data = konfig.getAll()
+
+    for _,s in pairs(data) do
+        if s.name == setting then
+            s.value = newValue
+            local final = textutils.serialise(data)
+            local f = fs.open(".konfig_settings", "w")
+            f.write(final)
+            f.close()
+
+            return
+        end
+    end
+
+    local newSetting = {}
+    newSetting.name = setting
+    newSetting.value = newValue
+    table.insert(data, newSetting)
+    local final = textutils.serialise(data)
+    local f = fs.open(".konfig_settings", "w")
+    f.write(final)
+    f.close()
+end
+
+function konfig.remove(setting)
+    local data = konfig.getAll()
+
+    for i,s in pairs(data) do
+        if s.name == setting then
+            table.remove(data, i)
+            local final = textutils.serialise(data)
+            local f = fs.open(".konfig_settings", "w")
+            f.write(final)
+            f.close()
+
+            return
+        end
+    end
+end
+
+return konfig

8184cd63abd7c86f97d42f215c886504abcaf2f4
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-01-18 16:29:22 -0500

    Update installer.lua

--- os/boot/installer.lua
+++ os/boot/installer.lua
@@ -1,86 +1,34 @@
-local konfig
-local gui
+local konfig = require("/apis/konfig")
+local gui = require("/apis/ecore_gui")
 
 local apps = {
     "SecureDoor",
     "SecureServer",
     "SecureController"
 }
+
 local app = ""
 
-local url = "https://raw.githubusercontent.com/SuperRavenSn1per/ECoreOS/refs/heads/main/"
-
-local currentLine = 4
+local url = "https://raw.githubusercontent.com/SuperRavenSn1per/ECoreOS/refs/heads/experimental/apps/"
 
 local function install(file, fileName)
     local dat = http.get(url .. file).readAll()
     local f = fs.open(fileName, "w")
     f.write(dat)
     f.close()
+    gui.printFormatted("Installed ", {"GITHUB/" .. file .. " ", colors.lightGray}, "as ", {fileName, colors.lightGray})
 end
-
-local function pre_install(file, fileName)
-    install(file, fileName)
-    print(file .. " -> " .. fileName)
-end
-
-local function appInstall(file)
-    install("apps/" .. app .. "/" .. file, file)
-    gui.writeFormatted(currentLine, {file, colors.lime}, " installed!")
-    currentLine = currentLine + 1
-end
-
-local function log(txt)
-    gui.write(currentLine, txt)
-    currentLine = currentLine + 1
-end
-
-if not fs.exists("/boot/boot_1.lua") then
-    term.clear()
-    term.setCursorPos(1,1)
-    print("ECoreOS Installer\n")
-    print("Would you like to install ECoreOS? (Y/n)\n")
-    while true do
-        write("> ")
-        local input = read()
-        if string.lower(input) == "y" then
-            print("Installing files...")
-            pre_install("os/startup.lua", "/startup.lua")
-            pre_install("os/boot/boot_1.lua", "/boot/boot_1.lua")
-            pre_install("os/boot/configuration.lua", "/boot/configuration.lua")
-            pre_install("os/boot/installer.lua", "/boot/installer.lua")
-            pre_install("os/apis/ecore_gui.lua", "/apis/ecore_gui.lua")
-            pre_install("os/apis/konfig.lua", "/apis/konfig.lua")
-            konfig = require("/apis/konfig")
-            print("Setting default configuration...")
-            konfig.set("require_modem", false)
-            konfig.set("require_host", false)
-            konfig.set("host_id", 0)
-            print("Installation complete! Rebooting...")
-            sleep(3)
-            os.reboot()
-        elseif string.lower(input) == "n" then
-            term.clear()
-            term.setCursorPos(1,1)
-            print("Installation cancelled!")
-            break
-        else
-            print("Invalid input. Please try again!")
-        end
-    end
-end
-
-konfig = require("/apis/konfig")
-gui = require("/apis/ecore_gui")
 
 gui.setPrimary(term.current())
 
 gui.clear()
 gui.title(_G.name .. " v" .. _G.version .. " - App Installer", colors.blue)
-gui.write(3, "[BACKSPACE] to boot OS")
-gui.write(5, "Please select an app below to install:")
+gui.writeLine(3, "[BACKSPACE] to return to boot menu.")
+gui.writeLine(5, "Please select an app below to install:")
+
+gui.setPos(1,7)
 for i,cApp in pairs(apps) do
-    gui.writeFormatted(6 + i, {tostring(i) .. ". ", colors.lightGray}, cApp)
+    gui.printFormatted({tostring(i) .. ". ", colors.lightGray}, cApp)
 end
 
 while true do 
@@ -90,28 +38,25 @@ while true do
         if app then
             gui.clear()
             gui.title(_G.name .. " v" .. _G.version .. " - Installing " .. app .. "...", colors.blue)
-            gui.primary.setCursorPos(1,3)
-            log("Installing files...")
-            appInstall("main.lua")
-            local data = http.get(url .. "apps/" .. app .. "/" .. "data").readAll()
-            local fData = textutils.unserialise(data)
-            local extraFiles = fData.extra_files
-            local config = fData.default_config
-            for i,file in pairs(extraFiles) do
-                appInstall(file)
+            gui.setPos(1, 3)
+            install(app .. "/" .. "main.lua", "main.lua")
+            local data = textutils.unserialise(http.get(url .. app .. "/" .. "data").readAll())
+            for i,extra in pairs(data.extra_files) do
+                install(app .. "/" .. extra, extra)
             end
-            log("Files installed! Setting default configuration...")
-            for i,setting in pairs(config) do
-                konfig.set(setting.name, setting.value)
+            gui.print("Main installation complete. Setting up configuration...")
+            for i,config in pairs(data.default_config) do
+                konfig.set(config.name, config.value)
             end
-            log("Configuration complete! Rebooting...")
+            for i,req in pairs(data.required) do
+                konfig.require(req)
+            end
+            gui.print("Installation complete. Rebooting...")
             sleep(3)
             os.reboot()
         end
     elseif event == "key" and char == 259 then
-        shell.run("/boot/boot_1.lua")
+        shell.run("/startup.lua")
         break
     end
 end
-
-

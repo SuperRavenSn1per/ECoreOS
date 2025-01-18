88688f251b3e1573cd286e1a30e0477875d1178a
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-01-18 15:56:12 -0500

    Update ecore_gui.lua

--- os/apis/ecore_gui.lua
+++ os/apis/ecore_gui.lua
@@ -1,18 +1,12 @@
 local gui = {}
 
 gui.primary = nil
-
-gui.bgColor = nil
-
 gui.w = 0
 gui.h = 0
+gui.bgColor = colors.black
 
-gui.objects = {
-    inputs = {}
-}
-
-gui.currentSelection = nil
-
+gui.buttons = {}
+gui.buttons.list = {}
 
 function gui.setPrimary(screen, color)
     local w,h
@@ -27,13 +21,90 @@ function gui.setPrimary(screen, color)
     end
 end
 
-local function skipLine()
-    local x,y = gui.primary.getCursorPos()
-    gui.primary.setCursorPos(1, y + 1)
+function gui.write(txt)
+    gui.primary.write(txt)
 end
 
-function gui.write(str)
-    gui.primary.write(str)
+function gui.setBG(color)
+    gui.primary.setBackgroundColor(color)
+end
+
+function gui.setFG(color)
+    gui.primary.setTextColor(color)
+end
+
+function gui.setPos(x, y)
+    gui.primary.setCursorPos(x, y)
+end
+
+function gui.getPos()
+    local x, y = gui.primary.getCursorPos()
+    
+    return x, y
+end
+
+local function skipLine()
+    local x, y = gui.getPos()
+    gui.setPos(1, y + 1)
+end
+
+function gui.clearLine(line, color)
+    gui.setPos(1, line)
+    if color then
+        gui.setBG(color)
+    end
+    for i = 1,gui.w do
+        gui.write(" ")
+    end
+    gui.setBG(gui.bgColor)
+    gui.setPos(1,line)
+end
+
+function gui.clear(color)
+    for i = 1,gui.h do
+        gui.clearLine(i, color or nil)
+    end
+end
+
+function gui.clearBox(startY, endY, color)
+    for i = startY,endY do
+        gui.clearLine(i, color or nil)
+    end
+end
+
+function gui.centerWrite(txt, line, offset)
+    if line == true then
+        line = gui.h / 2 + offset
+    end
+    gui.setPos(math.ceil(gui.w / 2 - string.len(txt) / 2), line)
+    gui.write(txt)
+end
+
+function gui.title(txt, color)
+    gui.clearLine(1, color)
+    gui.setBG(color)
+    gui.centerWrite(txt, 1)
+    gui.setBG(gui.bgColor)
+end
+
+function gui.writeLine(line, txt)
+    gui.clearLine(line)
+    gui.write(txt)
+end
+
+function gui.writeFormatted(line, ...)
+    local formatting = {...}
+
+    gui.clearLine(line)
+    for i,dat in pairs(formatting) do
+        if type(dat) == "table" then
+            gui.setFG(dat[2])
+            gui.write(dat[1])
+            gui.setFG(colors.white)
+        else
+            gui.write(dat)
+        end
+    end
 end
 
 function gui.print(str)
@@ -42,7 +113,7 @@ function gui.print(str)
        table.insert(words, word)
     end
     for i,word in pairs(words) do
-        local x = gui.primary.getCursorPos()
+        local x = gui.getPos()
         if x + #word > gui.w then
             skipLine()
         end
@@ -52,277 +123,97 @@ function gui.print(str)
     skipLine()
 end
 
-function gui.writeLine(line, str)
-    gui.clearLine(line)
-    gui.primary.setCursorPos(1, line)
-    gui.write(str)
-end
-
-function gui.writeFormatted(...)
-    local formatting = {...}
-
-    for i,dat in pairs(formatting) do
-        if type(dat) == "table" then
-            gui.primary.setTextColor(dat[2])
-            gui.write(dat[1])
-            gui.primary.setTextColor(colors.white)
-        else
-            gui.write(dat)
-        end
-    end
-end
-
-function gui.writeLineFormatted(line, ...)
-    local formatting = {...}
-
-    gui.clearLine(line)
-    gui.primary.setCursorPos(1, line)
-    for i,dat in pairs(formatting) do
-        if type(dat) == "table" then
-            gui.primary.setTextColor(dat[2])
-            gui.write(dat[1])
-            gui.primary.setTextColor(colors.white)
-        else
-            gui.write(dat)
-        end
-    end
-end
-
 function gui.printFormatted(...)
     local formatting = {...}
 
     for i,dat in pairs(formatting) do
         if type(dat) == "table" then
-            gui.primary.setTextColor(dat[2])
+            gui.setFG(dat[2])
             local words = {}
             for word in dat[1]:gmatch("[^%s]+") do
                table.insert(words, word)
             end
             for i,word in pairs(words) do
-                local x = gui.primary.getCursorPos()
+                local x = gui.getPos()
                 if x + #word > gui.w then
                     skipLine()
                 end
                 gui.write(word)
-                gui.write(i == #words and not dat[3] and "" or " ")
+                if dat[1]:sub(#dat[1], #dat[1]) == " " then
+                    gui.write(" ")
+                else
+                    gui.write(i == #words and "" or " ")
+                end
             end
-            gui.primary.setTextColor(colors.white)
+            gui.setFG(colors.white)
         else
             local words = {}
             for word in dat:gmatch("[^%s]+") do
                table.insert(words, word)
             end
             for i,word in pairs(words) do
-                local x = gui.primary.getCursorPos()
+                local x = gui.getPos()
                 if x + #word > gui.w then
                     skipLine()
                 end
                 gui.write(word)
-                gui.write(i == #words and "" or " ")
+                if dat:sub(#dat, #dat) == " " then
+                    gui.write(" ")
+                else
+                    gui.write(i == #words and "" or " ")
+                end
             end
         end
     end
     skipLine()
 end
 
+function gui.buttons.add(label, txt, x, y, color, hlColor, func, arg)
+    local button = {}
+    button.label = label
+    button.text = txt
+    button.x = x
+    button.y = y
+    button.color = color
+    button.highlight = hlColor
+    button.func = func
+    button.arg = arg
 
-function gui.clearLine(line)
-    gui.primary.setCursorPos(1, line)
-    for i = 1, gui.w do
-        gui.write(" ")
-    end
+    table.insert(gui.buttons.list, button)
 end
 
-function gui.clear()
-    for i = 1,gui.h do
-        gui.clearLine(i)
-    end
-end
-
-function gui.setTitle(title, color)
-    if color then
-        gui.primary.setBackgroundColor(color)
-    end
-    gui.clearLine(1)
-    gui.primary.setCursorPos(math.ceil(gui.w / 2 - #title / 2), 1)
-    gui.primary.write(title)
-    gui.primary.setBackgroundColor(gui.bgColor)
-end
-
-function gui.addObject(objType, objLabel, ...)
-    local args = {...}
-    local object = {}
-
-    if objType == "input" then
-        object.type = objType
-        object.label = objLabel
-        object.length = args[1]
-        object.x = args[2]
-        object.y = args[3]
-        object.color = args[4]
-        if args[6] then
-            local inTable = {}
-            for char in args[6]:gmatch("[^.]") do
-                table.insert(inTable, char)
-            end
-            args[6] = inTable
-        end
-        object.func = args[5]
-        object.input = args[6] or {}
-        object.enabled = false
-        object.maxX = object.x + object.length
-    elseif objType == "button" then
-        object.type = objType
-        object.label = objLabel
-        object.text = args[1]
-        object.x = args[2]
-        object.y = args[3]
-        object.color = args[4]
-        object.hlColor = args[5]
-        object.enabled = false
-        object.func = args[7]
-        object.maxX = object.x + #object.text + 2
-    end
-
-    table.insert(gui.objects, object)
-end
-
-function gui.drawObject(identifier)
-    if identifier == "all" then
-        for i,object in ipairs(gui.objects) do
-            gui.primary.setCursorPos(object.x, object.y)
-            gui.primary.setBackgroundColor(object.color)
-            if object.type == "input" then
-                for i = 1,object.length do
-                    gui.primary.write(" ")
-                end
-                gui.primary.setCursorPos(object.x, object.y)
-                for i,char in pairs(object.input) do
-                    write(char)
-                end
-                object.enabled = true
-            elseif object.type == "button" then
-                gui.primary.write(" " .. object.text .. " ")
-                object.enabled = true
-            end
-        end
-    elseif identifier == "input" or identifier == "button" then
-        for i,object in ipairs(gui.objects) do
-            if object.type == identifier then
-                gui.primary.setCursorPos(object.x, object.y)
-                gui.primary.setBackgroundColor(object.color)
-                if object.type == "input" then
-                    for i = 1,object.length do
-                        gui.primary.write(" ")
-                    end
-                    gui.primary.setCursorPos(object.x, object.y)
-                    for i,char in pairs(object.input) do
-                        write(char)
-                    end
-                    object.enabled = true
-                elseif object.type == "button" then
-                    gui.primary.write(" " .. object.text .. " ")
-                    object.enabled = true
-                end
-            end
-        end
-    else
-        for i,object in ipairs(gui.objects) do
-            if object.label == identifier then
-                gui.primary.setCursorPos(object.x, object.y)
-                gui.primary.setBackgroundColor(object.color)
-                if object.type == "input" then
-                    for i = 1,object.length do
-                        gui.primary.write(" ")
-                    end
-                    gui.primary.setCursorPos(object.x, object.y)
-                    for i,char in pairs(object.input) do
-                        write(char)
-                    end
-                    object.enabled = true
-                elseif object.type == "button" then
-                    gui.primary.write(" " .. object.text .. " ")
-                    object.enabled = true
-                end
-            end
+function gui.buttons.draw(label, hl)
+    local oldColor = gui.primary.getBackgroundColor()
+    for i,button in pairs(gui.buttons.list) do
+        if button.label == label then
+            gui.setPos(button.x, button.y)
+            gui.setBG(hl and button.highlight or button.color)
+            gui.write(" " .. button.text .. " ")
+            gui.setBG(oldColor)
         end
     end
 end
 
-function gui.hideObject(identifier)
-    if identifier == "all" then
-        for i,object in ipairs(gui.objects) do
-            gui.primary.setBackgroundColor(gui.bgColor)
-            gui.primary.setCursorPos(object.x, object.y)
-            for i = object.x,object.maxX do
-                gui.primary.write(" ")
-            end
-            object.enabled = false
-        end
-    elseif identifier == "input" or identifier == "button" then
-        for i,object in ipairs(gui.objects) do
-            if object.type == identifier then
-                gui.primary.setBackgroundColor(gui.bgColor)
-                gui.primary.setCursorPos(object.x, object.y)
-                for i = object.x,object.maxX do
-                    gui.primary.write(" ")
-                end
-                object.enabled = false
-            end
-        end
-    else
-        for i,object in ipairs(gui.objects) do
-            if object.label == identifier then
-                gui.primary.setBackgroundColor(gui.bgColor)
-                gui.primary.setCursorPos(object.x, object.y)
-                for i = object.x,object.maxX do
-                    gui.primary.write(" ")
-                end
-                object.enabled = false
-            end
-        end
+function gui.buttons.highlight(label)
+    gui.buttons.draw(label, true)
+    sleep(0.1)
+    gui.buttons.draw(label)
+end
+
+function gui.buttons.drawAll()
+    for i,button in pairs(gui.buttons.list) do
+        gui.buttons.draw(button.label)
     end
 end
 
-function gui.update()
+function gui.buttons.update()
     while true do
-        local event, char, x, y = os.pullEvent()
-        if event == "monitor_touch" or event == "mouse_click" then
-            for i,object in ipairs(gui.objects) do
-                if x >= object.x and x <= object.maxX and y == object.y then
-                    if object.enabled == true then
-                        if object.type == "input" then
-                            gui.primary.setCursorPos(object.x + #object.input, object.y)
-                            gui.currentSelection = object
-                            gui.primary.setBackgroundColor(object.color)
-                        elseif object.type == "button" then
-                            object.func()
-                        end
-                    end
-                end
-            end
-        elseif event == "char" then
-            if gui.currentSelection then
-                if  #gui.currentSelection.input < gui.currentSelection.length then
-                    gui.primary.write(char)
-                    table.insert(gui.currentSelection.input, char)
-                end
-            end
-        elseif event == "key" then
-            if char == keys.backspace and gui.currentSelection then
-                if #gui.currentSelection.input > 0 then
-                    local x,y = gui.primary.getCursorPos()
-                    gui.primary.setCursorPos(x - 1, y)
-                    write(" ")
-                    gui.primary.setCursorPos(x - 1, y)
-                    table.remove(gui.currentSelection.input, #gui.currentSelection.input)
-                end
-            elseif char == keys.enter then
-                if gui.currentSelection then
-                    if gui.currentSelection.func then
-                        gui.currentSelection.func(gui.currentSelection.label, table.concat(gui.currentSelection.input))
-                    end
-                    gui.currentSelection = nil
+        local e, b, x, y = os.pullEvent()
+        if e == "mouse_click" or e == "monitor_touch" then
+            for i,button in pairs(gui.buttons.list) do
+                if x >= button.x and x <= button.x + string.len(button.text) + 1 and y == button.y then
+                    gui.buttons.highlight(button.label)
+                    button.func(button.arg)
                 end
             end
         end

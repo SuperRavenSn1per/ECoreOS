cea94457c5264593182e9334f2793ff95aff38e4
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-01-18 15:55:16 -0500

    Update startup.lua

--- os/startup.lua
+++ os/startup.lua
@@ -1,52 +1,50 @@
 _G.name = "ECoreOS"
 _G.version = "1.0"
-_G.credit = "Made by EBM Technologies"
 
-local do_boot = true
-local program
+local gui = require("/apis/ecore_gui")
 
-local eutils = require("/apis/ecore_gui")
+gui.setPrimary(term.current())
 
-eutils.setPrimary(term.current())
+local boot = ""
+
+local selections = {
+    {"Boot OS", "/boot/boot_1.lua"},
+    {"Configuration", "/boot/configuration.lua"},
+    {"App Installer", "/boot/installer.lua"}
+}
 
 local function countdown()
     local t = 5
     repeat
-        eutils.write(9, "OS booting in " .. t .. "...")
+        gui.writeLine(5 + #selections + 1, "Booting OS in " .. tostring(t) .. "...")
         sleep(1)
         t = t - 1
     until t == 0
+    boot = "/boot/boot_1.lua"
+
+    return
 end
 
-local function selection()
+local function makeSelection()
     while true do
         local event, char = os.pullEvent("char")
-        if char == "1" then
-            break
-        elseif char == "2" then
-            do_boot = false
-            program = "/boot/configuration.lua"
-            break
-        elseif char == "3" then
-            do_boot = false
-            program = "/boot/installer.lua"
-            break
+        if selections[tonumber(char)] then
+            boot = selections[tonumber(char)][2]
+
+            return
         end
     end
 end
 
-eutils.clear()
-eutils.title(_G.name .. " v" .. _G.version .. " - Boot Menu", colors.blue)
-local w,h = term.getSize()
-eutils.write(h, _G.credit)
-eutils.write(3, "Press number key of option below:")
-eutils.writeFormatted(5, {"1. ", colors.lightGray}, "Boot OS")
-eutils.writeFormatted(6, {"2. ", colors.lightGray}, "Configuration")
-eutils.writeFormatted(7, {"3. ", colors.lightGray}, "App Installer")
+gui.clear()
+gui.title(_G.name .. " v" .. _G.version .. " - Boot Menu", colors.blue)
+gui.writeLine(3, "Press number key of selection below:")
+gui.setPos(1, 5)
 
-parallel.waitForAny(countdown, selection)
-if do_boot then 
-    shell.run("/boot/boot_1.lua")
-else
-    shell.run(program)
+for i,selection in pairs(selections) do
+    gui.printFormatted({i .. ". ", colors.lightGray}, selection[1])
 end
+
+parallel.waitForAny(countdown, makeSelection)
+
+shell.run(boot)

67b0d897d7a3fb78a80d7c54f6ef15a0f1826868
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-01-20 14:14:07 -0500

    Update startup.lua

--- os/startup.lua
+++ os/startup.lua
@@ -7,6 +7,8 @@ gui.setPrimary(term.current())
 
 local boot = ""
 
+local currentIndex = 1
+
 local selections = {
     {"Boot OS", "/boot/boot_1.lua"},
     {"Configuration", "/boot/configuration.lua"},
@@ -25,25 +27,52 @@ local function countdown()
     return
 end
 
+local function drawSelection(index)
+    gui.setPos(1, 5)
+    for i,selection in pairs(selections) do
+        if i == index then
+            gui.setBG(colors.gray)
+        end
+        gui.printFormatted({i .. ". ", colors.lightGray}, selection[1] .. " ")
+        gui.setBG(gui.bgColor)
+    end
+end
+
 local function makeSelection()
     while true do
-        local event, char = os.pullEvent("char")
-        if selections[tonumber(char)] then
-            boot = selections[tonumber(char)][2]
-
-            return
+        local event, char = os.pullEvent()
+        if event == "char" then
+            if char == "w" then
+                currentIndex = currentIndex - 1
+                if currentIndex < 1 then
+                    currentIndex = #selections
+                end
+                drawSelection(currentIndex)
+            elseif char == "s" then
+                currentIndex = currentIndex + 1
+                if currentIndex > #selections then
+                    currentIndex = 1
+                end
+                drawSelection(currentIndex)
+            elseif tonumber(char) then
+                if selections[tonumber(char)] then
+                    boot = selections[tonumber(char)][2]
+                    break
+                end
+            end
+        elseif event == "key" then
+            if char == 257 then
+                boot = selections[currentIndex][2]
+                break
+            end
         end
     end
 end
 
 gui.clear()
 gui.title(_G.name .. " v" .. _G.version .. " - Boot Menu", colors.blue)
-gui.writeLine(3, "Press number key of selection below:")
-gui.setPos(1, 5)
-
-for i,selection in pairs(selections) do
-    gui.printFormatted({i .. ". ", colors.lightGray}, selection[1])
-end
+gui.writeLine(3, "Make a selection below:")
+drawSelection(currentIndex)
 
 parallel.waitForAny(countdown, makeSelection)
 

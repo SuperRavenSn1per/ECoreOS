7a7c17b41a7acbf71e7f300d6f9295d98a9c2776
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-01-21 14:01:14 -0500

    Update installer.lua

--- os/boot/installer.lua
+++ os/boot/installer.lua
@@ -56,7 +56,7 @@ while true do
             os.reboot()
         end
     elseif event == "key" and char == 259 then
-        shell.run("/startup.lua")
+        shell.run("/boot_1.lua")
         break
     end
 end

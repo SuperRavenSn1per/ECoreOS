83260fd664fd6ac2f6c51db48e626aa62595a8f1
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-02-05 15:46:41 -0500

    Update startup.lua

--- os/startup.lua
+++ os/startup.lua
@@ -1,27 +1,21 @@
-_G.name = "ECoreOS"
-_G.version = "1.0"
+_G.os_name = "ECoreOS"
+_G.os_version = "1.0"
 
-local gui = require("/apis/ecore_gui")
 local konfig = require("/apis/konfig")
 
 gui.setPrimary(term.current())
 
 if konfig.get("require_login") == true then
-    gui.clear()
-    gui.title(_G.name .. " v" .. _G.version .. " - Login", colors.red)
-    gui.writeLine(3, "Username: ")
-    gui.writeLine(4, "Password: ")
-    gui.setPos(1 + string.len("Username: "), 3)
+    term.setBackgroundColor(colors.red)
+    term.clear()
+    term.setCursorPos(1,1)
+    print(os_name .. " v" .. os_version)
+    print("Login Page\n")
+    write("Username > ")
     local username = read()
-    gui.setPos(1 + string.len("Password: "), 4)
-    local password = read("*")
-    if username ~= konfig.get("username") and password ~= konfig.get("password") then
-        gui.writeFormatted(6, {"Incorrect username or password!", colors.red})
-        sleep(3)
-        os.reboot()
-    else
-        shell.run("/boot/boot_1.lua")
-    end
+    print("")
+    write("Password > ")
+    local password = read()
 else
     shell.run("/boot/boot_1.lua")
 end

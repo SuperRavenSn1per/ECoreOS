2ff69f4187655f93f113170fa9acefafe73631c9
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-02-05 16:00:28 -0500

    Update startup.lua

--- os/startup.lua
+++ os/startup.lua
@@ -5,14 +5,42 @@ local konfig = require("/apis/konfig")
 
 if konfig.get("require_login") == true then
     term.setBackgroundColor(colors.red)
+    if not fs.exists("/.account_info") then
+        term.clear()
+        term.setCursorPos(1,1)
+        print("----------------")
+        print("| " .. _G.name .. " v" .. _G.version .. " |")
+        print("----------------\n")
+        print("'REQUIRE_LOGIN' is set to true but no account has been created. Please create one below.\n")
+        write("New Username > ")
+        local newUser = read()
+        write("New Password > ")
+        local newPass = read()
+        local info = {}
+        info.username = newUser
+        info.password = newPass
+        local f = fs.open("/.account_info", "w")
+        f.write(textutils.serialise(info))
+        f.close()
+    end
     term.clear()
     term.setCursorPos(1,1)
-    print(_G.name .. " v" .. _G.version)
-    print("Login Page\n")
+    print("----------------")
+    print("| " .. _G.name .. " v" .. _G.version .. " |")
+    print("----------------\n")
+    print("Please enter the correct username and password below.\n")
     write("Username > ")
     local username = read()
     write("Password > ")
-    local password = read()
+    local password = read("*")
+    local f = fs.open("/.account_info", "r")
+    local accountInfo = textutils.unserialise(f.readAll())
+    if accountInfo.username == username and accountInfo.password == password then
+        shell.run("/boot/boot_1.lua")
+    else
+        print("\nUsername or password is incorrect!")
+        shell.run("/startup.lua")
+    end
 else
     shell.run("/boot/boot_1.lua")
 end

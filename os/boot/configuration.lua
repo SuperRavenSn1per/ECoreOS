d10219d3ab5076af4d4e2468fef54ccd5bd4fb3c
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-02-06 06:53:26 -0500

    Update configuration.lua

--- os/boot/configuration.lua
+++ os/boot/configuration.lua
@@ -3,16 +3,18 @@ local konfig = require("/apis/konfig")
 local currentIndex = 1
 
 local function drawConfig()
-    term.setSetCursorPos(1,5)
+    term.setCursorPos(1,6)
     for i,setting in pairs(konfig.getAll()) do
         if i == currentIndex then
-            print("> " .. setting.name)
+            print("> " .. string.upper(setting.name))
         else
-            print("  " .. setting.name)
+            print("  " .. string.upper(setting.name))
         end
     end
 end
 
 term.setBackgroundColor(colors.blue)
+tern.clear()
+tern.setCursorPos(1,1)
 print("EDITING CONFIGURATION\n")
 print("Press [BACKSPACE] to exit or select a setting to edit below.")

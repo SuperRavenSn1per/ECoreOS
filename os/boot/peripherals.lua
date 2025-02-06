1f047bb3db2eb992e337abd609e1d33db82b6494
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-02-06 07:33:16 -0500

    Update peripherals.lua

--- os/boot/peripherals.lua
+++ os/boot/peripherals.lua
@@ -5,7 +5,7 @@ local selection = konfig.getRequired()[currentIndex]
 
 local function drawRequired()
     term.setCursorPos(1,6)
-    print("Current Selection: " .. selection.name .. "          ")
+    print("Current Selection: " .. p .. "          ")
     term.setCursorPos(1,8)
     for i,p in pairs(konfig.getRequired()) do
         if i == currentIndex then

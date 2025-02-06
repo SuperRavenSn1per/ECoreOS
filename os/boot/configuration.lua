9a95aff10b464f47b342237ef5472d787565f723
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2025-02-06 06:57:53 -0500

    Update configuration.lua

--- os/boot/configuration.lua
+++ os/boot/configuration.lua
@@ -6,9 +6,9 @@ local function drawConfig()
     term.setCursorPos(1,6)
     for i,setting in pairs(konfig.getAll()) do
         if i == currentIndex then
-            print("> " .. string.upper(setting.name))
+            print("> " .. string.upper(setting.name) .. "     ")
         else
-            print("  " .. string.upper(setting.name))
+            print("  " .. string.upper(setting.name) .. "     ")
         end
     end
 end
@@ -18,3 +18,21 @@ tern.clear()
 tern.setCursorPos(1,1)
 print("EDITING CONFIGURATION\n")
 print("Press [BACKSPACE] to exit or select a setting to edit below.")
+
+while true do
+    local event, key = os.pullEvent()
+    if event == "char" then
+        if key == "w" then
+            currentIndex = currentIndex - 1
+            if currentIndex <= 0 then
+                currentIndex = #konfig.getAll()
+            end
+        elseif key == "s" then
+            currentIndex = currentIndex + 1
+            if currentIndex >= #konfig.getAll() then
+                currentIndex = 1
+            end
+        end
+        drawConfig()
+    end
+end
